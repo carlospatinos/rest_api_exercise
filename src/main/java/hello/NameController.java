0c0602b3433bf87b704a5ff1f93c48024a501fcd
Carlos Patino <carlos.patino_soria@genesys.com>
2019-10-23 15:59:59 +0100

    Adding wrapper and allowing cors

--- src/main/java/hello/NameController.java
+++ src/main/java/hello/NameController.java
@@ -2,7 +2,8 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.Random;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,10 @@ public class NameController {
 
     private static final String template = "Your name is: %s!";
     private final AtomicLong counter = new AtomicLong();
-    private final String[] names = {"Carlos", "Pedro", "Luis"};
+    private final String[] names = {"Carlos", "Pedro", "Luis", "Fabi", "Naila", "Joyce", "Rita"};
 
-    @RequestMapping("/showmyname")
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/showmyname")
     public Name showMyName(@RequestParam(value="name", defaultValue="") String name) {
     	int rnd = new Random().nextInt(names.length);
     	if (name == null || name.equals("")){
